ac20d66d4c3ecf16a66776dfcace5206ab35dab4
iamsyada <syadaruesyada@gmail.com>
2026-05-11 14:19:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const startBtn = document.getElementById("startBtn");
 
-const popup = document.getElementById("popup");
-
-const openBtn = document.getElementById("openBtn");
-
 const opening = document.getElementById("opening");
 
 const main = document.getElementById("main");
@@ -13,7 +9,7 @@ const bgm = document.getElementById("bgm");
 const video = document.querySelector(".bg-video");
 
 
-// STEP 1
+
 startBtn.addEventListener("click", async () => {
 
     try{
@@ -24,22 +20,14 @@ startBtn.addEventListener("click", async () => {
 
     }catch(err){
 
-        console.log("media blocked");
+        console.log(err);
 
     }
 
-    popup.classList.remove("hidden");
-
-});
-
-
-// STEP 2
-openBtn.addEventListener("click", () => {
 
     opening.style.display = "none";
 
     main.classList.remove("hidden");
-
 
 
     confetti({
@@ -49,49 +37,3 @@ openBtn.addEventListener("click", () => {
     });
 
 });
-  setTimeout(() => {
-
-    confetti({
-      particleCount:100,
-      angle:60,
-      spread:80,
-      origin:{ x:0 }
-    });
-
-    confetti({
-      particleCount:100,
-      angle:120,
-      spread:80,
-      origin:{ x:1 }
-    });
-
-  },500);
-
-});
-
-
-// TYPING EFFECT
-const text =
-`Happy Gradu-atee Sekencoyz! 💖
-
-Semua perjuangan, lelah, dan air mata akhirnya terbayar.
-Aku bangga melihatmu sampai di titik ini.
-
-Semoga langkahmu berikutnya dipenuhi kebahagiaan, kesuksesan, dan hal-hal indah lainnya ✨`;
-
-let i = 0;
-
-function typing(){
-
-  if(i < text.length){
-
-    document.getElementById("typing").innerHTML += text.charAt(i);
-
-    i++;
-
-    setTimeout(typing,45);
-  }
-
-}
-
-typing();
